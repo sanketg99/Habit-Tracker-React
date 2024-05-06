b12a2564453c62ca55507525c3801a7d3c629941
sanketg99 <sanketgorade99@gmail.com>
2024-05-06 19:52:48 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import WeekView from "./components/WeekView";
+
+const App = () => {
+  return (
+    <>
+      <Routes >
+        <Route path="/" element={<Home />} />
+        <Route path="/week-view" element={<WeekView />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
